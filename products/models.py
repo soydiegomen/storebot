521e7af164c3f03f789c7a669af40be96282edc0
Diego Mendoza <dmendoza@inflexionsoftware.com>
2021-05-25 19:16:49 -0500

    Agrega los campos del modelo Producto

--- products/models.py
+++ products/models.py
@@ -1,21 +1,33 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authentication import TokenAuthentication
-from django.contrib.auth.models import AbstractUser, User
-from django.conf import settings
+#from django_enumfield import enum
+#from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from storebot_api.models import Brand
+
+""" class Gender(enum.Enum):
+    WOMAN = 0
+    MAN = 1 """
 
 class Product(models.Model):
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=False, blank=False, default=None)
+    url = models.CharField(max_length=512,null=False, blank=False)
+    price = models.DecimalField(null=True, blank=True, max_digits=12, decimal_places=2)
+    discount = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=2)
+    final_price = models.DecimalField(null=True, blank=True, max_digits=12, decimal_places=2)
+    sku = models.CharField(max_length=255,null=False, blank=False)
     name = models.CharField(max_length=255,null=False, blank=False)
     description = models.CharField(max_length=255,null=True, blank=True)
     quantity = models.IntegerField(null=True, blank=True)
-    price = models.DecimalField(null=True, blank=True, max_digits=12, decimal_places=2)
+    order = models.IntegerField(null=True, blank=True, default=0)
+    views = models.IntegerField(null=False, blank=True, default=0)
+    instagram_likes = models.IntegerField(null=False, blank=True, default=0)
     is_active = models.BooleanField(null=False, blank=False, default=True)
-    image = models.ImageField(upload_to='image/%Y-%m-%d/', null=True, blank=False)
+    #gener = enum.EnumField(Gender, default=Gender.MAN)
+    #image = models.ImageField(upload_to='image/%Y-%m-%d/', null=True, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    updated_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def __str__(self):
         return self.name
